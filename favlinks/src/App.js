d7c2e04b420f5eaf7d6f273d5ad33ebc788b6b60
Natalie <natalie.dflcn@gmail.com>
2023-04-30 17:39:09 -0400

    Installed PostGres and initialized a database in Postgres

--- favlinks/src/App.js
+++ favlinks/src/App.js
@@ -2,7 +2,23 @@ import React from 'react'
 import LinkContainer from './components/LinkContainer'
 import './App.css'
 
+
 function App() {
+
+  const fetchAPI = async () => {
+    try {
+      let response = await fetch ('/api')
+      let data = await response.json()
+      setMessage(data.message)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  }, [])
+  
   return (
     <div className="App">
       <LinkContainer />
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const bodyParser = require('body-parser');
+
 const path = require('path');
 
 const PORT = 8000;
@@ -8,7 +10,24 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../favlinks/build')))
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
 app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../favlinks/build", "index.html"))
+})
+
+//should return the links stored on your postgres database
+app.get('/links', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../favlinks/build", "index.html"))
+})
+
+//should retrieve a link by ID
+app.get('/links/:id', (req, res) => {
     res.sendFile(path.resolve(__dirname, "../favlinks/build", "index.html"))
 })
 
